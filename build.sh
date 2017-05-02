63885faf5f795e8580fea85a125eab0b0ab2c37c
yya007 <yaoyuang1996@gmail.com>
2017-05-02 16:15:51 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@ sudo rmmod proc1;
 sudo rmmod proc2;
 sudo rmmod nf2
 cd proc;
-sudo dmesg -c
-make;
+sudo dmesg -c;
+sudo make;
 sudo insmod proc1.ko;
 sudo insmod proc2.ko;
 echo "127.0.1.1" >/proc/IP;
 echo "1" >/proc/Function ;
 cd ..;
 cd netfilter;
-make
+sudo make;
 sudo insmod nf2.ko
